6240802f9ec530c708b59efa8c2d4a46d92955c0
Alex Levinson <61521851+alcl00@users.noreply.github.com>
2023-08-04 17:22:35 +0000

    Updated code in App for efficiency

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -21,32 +21,10 @@ public class App
     // Challenge 2
     public static boolean isPasswordComplex(String password) {
         
-        if(password.length() < 6) {
-            return false;
-        }
-
-        boolean hasLowerCase = false;
-        boolean hasUpperCase = false;
-        boolean hasNumber = false;
-
-        for(int i = 0; i < password.length(); i++) {
-            char current = password.charAt(i);
-            if(!hasLowerCase && Character.isLowerCase(current)) {
-                hasLowerCase = true;
-            }
-            else if(!hasUpperCase && Character.isUpperCase(current)) {
-                hasUpperCase = true;
-            }
-            else if(!hasNumber && Character.isDigit(current)) {
-                hasNumber = true;
-            }
-
-            // Kills for-loop if all conditions are met
-            if(hasLowerCase && hasUpperCase && hasNumber) {
-                break;
-            }
-        }
         
-        return hasLowerCase && hasUpperCase && hasNumber;
+        return password.length() >= 6 
+        && password.matches(".*[A-Z].*")
+        && password.matches(".*[a-z].*")
+        && password.matches(".*\\D.*");
     }
 }
