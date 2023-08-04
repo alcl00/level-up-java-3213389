07387ed47a4135de77c048a8038ff6eda7147045
Alex Levinson <61521851+alcl00@users.noreply.github.com>
2023-08-04 17:13:42 +0000

    Added code for challenge 2

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,5 +1,7 @@
 package com.linkedin.javacodechallenges;
 
+import java.util.regex.Pattern;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,42 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    // Challenge 1
+    public static boolean isEven(int n) {
+        return n % 2 == 0;
+    }
+
+    // Challenge 2
+    public static boolean isPasswordComplex(String password) {
+        
+        if(password.length() < 6) {
+            return false;
+        }
+
+        boolean hasLowerCase = false;
+        boolean hasUpperCase = false;
+        boolean hasNumber = false;
+
+        for(int i = 0; i < password.length(); i++) {
+            char current = password.charAt(i);
+            if(!hasLowerCase && Character.isLowerCase(current)) {
+                hasLowerCase = true;
+            }
+            else if(!hasUpperCase && Character.isUpperCase(current)) {
+                hasUpperCase = true;
+            }
+            else if(!hasNumber && Character.isDigit(current)) {
+                hasNumber = true;
+            }
+
+            // Kills for-loop if all conditions are met
+            if(hasLowerCase && hasUpperCase && hasNumber) {
+                break;
+            }
+        }
+        
+        return hasLowerCase && hasUpperCase && hasNumber;
     }
 }
--- javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
+++ javacodechallenges/src/test/java/com/linkedin/javacodechallenges/AppTest.java
@@ -1,5 +1,6 @@
 package com.linkedin.javacodechallenges;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -9,12 +10,49 @@ import org.junit.Test;
  */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
+    // Tests for code challenge 1 isEven()
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void isEven_true() {
+        assertTrue(App.isEven(0));
+        assertTrue(App.isEven(2));
+        assertTrue(App.isEven(4));
+        assertTrue(App.isEven(6));
+        assertTrue(App.isEven(8));
+        assertTrue(App.isEven(-20));
+        assertTrue(App.isEven(100));
+        assertTrue(App.isEven(-46));
+    }
+
+    @Test
+    public void isEven_false() {
+        assertFalse(App.isEven(1));
+        assertFalse(App.isEven(3));
+        assertFalse(App.isEven(5));
+        assertFalse(App.isEven(7));
+        assertFalse(App.isEven(9));
+        assertFalse(App.isEven(-11));
+        assertFalse(App.isEven(93));
+        assertFalse(App.isEven(-75));
+    }
+
+    // Tests for code challenge 2 password complexity
+    @Test
+    public void isPasswordComplex_true() {
+        assertTrue(App.isPasswordComplex("Happy12"));
+        assertTrue(App.isPasswordComplex("1SarahL ength"));
+        assertTrue(App.isPasswordComplex("Password12"));
+        assertTrue(App.isPasswordComplex("2PassW@ord1"));
+        assertTrue(App.isPasswordComplex("01Sall#y9"));
+    }
+
+    @Test
+    public void isPasswordComplex_false() {
+        assertFalse(App.isPasswordComplex("12aB "));
+        assertFalse(App.isPasswordComplex("Ab112"));
+        assertFalse(App.isPasswordComplex("happy"));
+        assertFalse(App.isPasswordComplex("happy_1"));
+        assertFalse(App.isPasswordComplex("p assword12"));
+        assertFalse(App.isPasswordComplex("Sur18"));
+        assertFalse(App.isPasswordComplex(""));
     }
 }
